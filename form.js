3eabd93eee0c0d53fd2035f0fa8e05ba0cfa0d43
ElamaranDev <marandev007@gmail.com>
2023-06-01 18:05:11 +0530

    form data will be stored in spread Sheets

--- form.js
+++ form.js
@@ -1,49 +1,13 @@
-const form = document.getElementById("form1");
-form.addEventListener("submit", handleSubmit);
-function handleSubmit(event){
-    event.preventDefault();
-    const name=document.getElementById('name').value;
-    const age= document.getElementById('age').value;
-    const college = document.getElementById('clg').value;
-    const program = document.getElementById('pgm').value;
-    const register = document.getElementById('reg').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('pno').value;
-    const year = document.getElementById('year').value;
 
-    var errorMessage ="";
-    if(!validateEmail(email)){
-        errorMessage+= "Invalid email format.\n";
-    }
-    if (!validatePhone(phone)) 
-    {
-        errorMessage += "Invalid phone number.\n";
-    }
-    if (errorMessage !== "") 
-    {
-        alert(errorMessage);
-    } 
-    else {
-        const wb = XLSX.utils.book_new();
-        const ws = XLSX.utils.json_to_sheet([{
-        Name: name,
-        Email: email,
-        Age: age,
-        College: college,
-        Program: program,
-        Registration_No: register,
-        Phone_no: phone,
-        Academic_year: year }]);
-        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-        XLSX.writeFile(wb, 'Student_data.xlsx');
-        form.reset();
-    }
-}
-function validateEmail(email){
-    var re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-function validatePhone(phone) {
-    var re = /^\d{10}$/; 
-    return re.test(phone);
-}
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwVXEIkKXhc47uOYiRnIQSGILTs8GmRjxy4g99j3JG7p53gH6fT6KHkLrlgKuUr0LBv/exec'
+  const form = document.forms['Student-Information']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+      form.reset();
+      window.location.href="success.html";
+  })
+
